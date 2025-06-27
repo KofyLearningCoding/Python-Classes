fbee7ce3ca0fee092cafec4019bc93ff5b3b6bb6
KofyLearningCoding <kofyosei9@gmail.com>
2025-06-27 19:58:01 +0000

    11

---
+++ Getting started with Programming/main.py
@@ -0,0 +1,13 @@
+print("welcome to the world of coding!!\n this is my first code in python ")
+print("this is my first code in python")
+#create variable
+student_name="fart" #string
+student_age=1000000000 #integer
+#input statement
+parent_name=input("Enter parent name: ")
+parent_age=input("enter parent age:")
+#output statement
+print ("Student name is: ",student_name)
+print ("Student age is: ",student_age)
+print ("Parent name is: ",parent_name)
+print ("Parent age is: ",parent_age)
