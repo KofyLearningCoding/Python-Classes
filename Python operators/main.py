90bfd198fa4f0306df4f330c2e29e62d846ad13b
KofyLearningCoding <kofyosei9@gmail.com>
2025-07-11 19:54:41 +0000

    ggggg

---
+++ Python operators/main.py
@@ -0,0 +1,28 @@
+#declare 2 variables
+num1=25
+num2=32
+#arithmetic Operators
+# additional 
+print(f"num1 + num2={num1+num2}")
+#subtraction
+print(f"num1 - num2={num1-num2}")
+#multiplication
+print(f"num1 * num2={num1*num2}")
+#division
+print(f"num2 / num1={num2/num1}")
+#modulus
+print(f"num2 % num1={num2%num1}")
+#exponential
+print(f"num1 ** num2={num1**num2}")
+#floor division
+print(f"num1 // num2={num1//num2}")
+
+#comparison operator
+#equal to 
+print(f"num1 is equal to  num2:{num1==num2}")
+#less than
+print(f"num1 is less than num2:{num1<num2}")
+#greater than
+print(f"num1 is greater than num2:{num1>num2}")
+#not equal to
+print(f"num1 is not equal to  num2:{num1!=num2}")
