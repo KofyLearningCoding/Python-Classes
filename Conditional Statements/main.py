71aea6cd05b61e555829a575cfc18fa8c289c325
KofyLearningCoding <kofyosei9@gmail.com>
2025-07-12 19:46:20 +0000

    28637325

---
+++ Conditional Statements/main.py
@@ -0,0 +1,3 @@
+number=3
+if number>0:
+    print(f"{number} is a positive number")
---
+++ Conditional Statements/main2.py
@@ -0,0 +1,7 @@
+buy_price=int(input("Enter the buying price: "))
+sell_price=int(input("Enter the sellingprice:"))
+
+if (sell_price>buy_price):
+    print("you have made a profit")
+else:
+    print("you have made a loss")
